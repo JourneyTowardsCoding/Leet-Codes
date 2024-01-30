3a1364d33ad78abcb9e495dabd47942cc4bafc94
Muhammad Tauhid <journey.towards.coding@gmail.com>
2024-01-30 18:54:34 +0600

    lc-10

---
+++ LC-10.js
@@ -0,0 +1,20 @@
+QUESTION://2666. Allow One Function Call
+
+// Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+
+// The first time the returned function is called, it should return the same result as fn.
+// Every subsequent time it is called, it should return undefined.
+ANS:
+
+var once = function(fn){
+  let called = false;
+  return function(...args){
+    if(called){
+      return undefined;
+    }
+    else{
+      called = true;
+      return fn.apply(this, args);
+    }
+  }
+}
