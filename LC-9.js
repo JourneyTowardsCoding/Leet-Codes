1e470a52997f12d5c55ebd4a78b69f1689324add
Muhammad Tauhid <journey.towards.coding@gmail.com>
2024-01-30 18:51:38 +0600

    lc-9

---
+++ LC-9.js
@@ -0,0 +1,7 @@
+QUESTION://2703. Return Length of Arguments Passed
+// Write a function argumentsLength that returns the count of arguments passed to it.
+
+ANS:
+var argumentsLength = function(...args){
+  return args.length;
+}
